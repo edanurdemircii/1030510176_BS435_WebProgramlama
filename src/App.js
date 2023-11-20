55195a48b638e8db1c6773eee67b438b00c54582
edanurdemircii <ednrdmrc9@gmail.com>
2023-11-20 15:55:49 +0300

    buton stilleri, tahmin yapma ve puan özelliği ekledim.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import OyunComponent from './components/OyunComponent/start';
 
 function App() {
   return (
       <div>
-        <h1>Sayı Tahmin Oyunu</h1>
         <OyunComponent />
+
       </div>
   );
 }
--- src/components/OyunComponent/start.js
+++ src/components/OyunComponent/start.js
@@ -1,21 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect} from 'react';
+import   "./start.css";
 
 function OyunComponent() {
-    const [oyunaBasladi, setOyunaBasladi] = useState(false);
+    const [sayı, setSayı] = useState(Math.floor(Math.random() * 10) + 1);
+    const [tahmin, setTahmin] = useState('');
+    const [geribildirim, setGeribildirim] = useState('');
+    const [puan, setPuan] = useState(0);
 
-    const baslaOyuna = () => {
-        setOyunaBasladi(true);
-    };
+    useEffect(() => {
+        if (puan === 50) {
+            oyunuSıfırla();
+        }
+    }, [puan]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const parsEdilenTahmin = parseInt(tahmin);
+
+        if (isNaN(parsEdilenTahmin)) {
+            setGeribildirim('Lütfen geçerli bir sayı girin.');
+        } else if (parsEdilenTahmin === sayı) {
+            setGeribildirim('Doğru tahmin. Tebrikler!');
+            setPuan(puan + 5); // Her doğru tahminde 5 puan ekle
+        } else if (parsEdilenTahmin > sayı) {
+            setGeribildirim('Tahmininizi azaltın.');
+        } else {
+            setGeribildirim('Tahmininizi arttırın.');
+        }
+        setTahmin(''); // Gönderimden sonra giriş alanını temizle
+    }
+
+    function oyunuSıfırla() {
+        setSayı(Math.floor(Math.random() * 10) + 1);
+        setTahmin('');
+        setGeribildirim('');
+        setPuan(0);
+    }
 
     return (
-        <div>
-            {!oyunaBasladi ? (
-                <button onClick={baslaOyuna}>Oyuna Başla</button>
-            ) : (
-                <p>Hadi Beni Tahmin Et</p>
-            )}
+        <div className="startBody">
+            <h1>OYUNA HOŞGELDİNİZ</h1>
+            <p>{geribildirim}</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    1 ile 10 arasında bir sayı tahmin edin:
+                    <input
+                        type="number"
+                        min="1"
+                        max="100"
+                        placeholder="Tahmininizi girin"
+                        value={tahmin}
+                        onChange={(event) => setTahmin(event.target.value)}
+                    />
+                </label>
+                <button type="submit">Tahmin Gönder</button>
+            </form>
+            <div id="score-frame">
+                <p id="score">Puan: {puan}</p>
+            </div>
+            <button onClick={oyunuSıfırla}>Oyunu Sıfırla</button>
         </div>
     );
 }
-
 export default OyunComponent;
